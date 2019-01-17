558017d4a28919fb2360d97526b10ed7102c3b94
Isaac Winters <isaacw@autometrix.local>
2019-01-17 10:44:11 -0800

    Includes two way biding and state modification

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Isaac", age: 28},
+      {name: "Alannah", age: 24},
+      {name: "Lancelot", age: 4}
+    ]
+  }
+
+  switchNameHandler = () => {
+    
+    this.setState({
+      persons:
+      [{name: "Isaac", age: 28},
+      {name: "Alannah", age: 24},
+      {name: "Lancelot", age: 30}]
+    })
+    
+    //Dont do this
+    //this.state.persons[0].name = "BillyLooWho"  
+    
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>React App</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> <li>Children Data</li> </Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> <li>Children Data</li> </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> <li>Children Data</li><li>Child data 2</li> </Person>
       </div>
     );
   }
